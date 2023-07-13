9ce848cad4dee54348bad543ecc50adf10da51ae
Vimal-Badoniya <vimal.badoniya.05@gmail.com>
2023-07-13 18:32:44 +0530

    first commit

--- src/components/UI/HeaderButton.js
+++ src/components/UI/HeaderButton.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import "./HeaderButton.css";
-// import { HiShoppingCart } from "react-icons/hi";
 import CartContext from "../../source/cart-context";
 
 
@@ -16,9 +15,6 @@ const HeaderButton = (props) => {
 
   return (
     <button className="cart-button" onClick={clickHandler} >
-      <span>
-        {/* <HiShoppingCart className="child-icon" /> */}
-      </span>
       <span className="child-text">Your Cart</span>
       <span className="counter">{ctx.quantity}</span>
     </button>
